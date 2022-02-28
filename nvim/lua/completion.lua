b5686b18f2c42e2590bbbe33c59a0e9831ba4c2c
s0LA1337 <dream@neoncity.dev>
2022-02-28 21:49:24 +0000

    try and apply lspconfig (doesn't work tho)

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,13 +1,13 @@
 local lspkind = require "lspkind"
 
-local lspconfig = require('lspconfig')
+local lspconfig = require "lspconfig"
 
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   require('completion').on_attach()
 end
 
-local servers = {'zls'}
+local servers = {'zls', 'rust_analyzer'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
 		on_attach = on_attach,
