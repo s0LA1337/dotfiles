60a8f03a4c0c788fd970192f5ec9d4b6c0a4ff25
s0LA1337 <dream@neoncity.dev>
2022-05-20 18:48:18 +0200

    Remove shade

--- nvim/lua/init_lsp.lua
+++ nvim/lua/init_lsp.lua
@@ -4,10 +4,7 @@ lcfg.zls.setup{}
 lcfg.ccls.setup{}
 lcfg.gopls.setup{}
 lcfg.rust_analyzer.setup{}
-lcfg.tsserver.setup{}
 lcfg.cmake.setup{}
 lcfg.dockerls.setup{}
 lcfg.html.setup{}
-lcfg.csharp_ls.setup{}
-lcfg.pyright.setup{}
 lcfg.rnix.setup{}
