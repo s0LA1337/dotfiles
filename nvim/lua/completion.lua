e29b336f0b05236e842bf6681ff6508c68618333
s0LA1337 <dreamer@neoncity.dev>
2022-05-25 23:19:59 +0000

    Update completion

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -4,62 +4,46 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 local cmp = require("cmp")
 
-cmp.setup {
-    mapping = {
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }),
-     	["<Tab>"] = function(fallback)
-			  if cmp.visible() then
-		      cmp.select_next_item()
-			  else
-			    fallback()
-			  end
-			end,
-			["<S-Tab>"] = function(fallback)
-			  if cmp.visible() then
-			    cmp.select_prev_item()
-			  else
-			    fallback()
-			  end
-			end,
-		},
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'path' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-    },
+      { name = 'buffer' }
+    }
+  })
 
-		snippet = {
-				expand = function(args) 
-				    require("luasnip").lsp_expand(args.body)
-				end,
-		},
-
-		formatting = {
-		  format = lspkind.cmp_format {
-		    with_text = true,
-        menu = {
-				  buffer   = "[BUF]",
-			 	  nvim_lsp = "[LSP]",
-          nvim_lua = "[API]",
-          path     = "[PATH]",
-          luasnip  = "[SNIP]",
-        },
-      },
-    },
-
-		experimental = {
-      native_menu = false,
-			ghost_text = true,
-		},
-}
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
