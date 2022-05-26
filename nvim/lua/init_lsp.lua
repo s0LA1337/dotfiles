ae2631e55ddc5490aeb1ff1df7ee8d23df708800
s0LA1337 <dreamer@neoncity.dev>
2022-05-26 01:01:28 +0000

    Test?

--- nvim/lua/init_lsp.lua
+++ nvim/lua/init_lsp.lua
@@ -1,5 +1,23 @@
 local lcfg = require('lspconfig')
 
+local lsp_defaults = {
+  flags = {
+    debounce_text_changes = 150,
+  },
+  capabilities = require('cmp_nvim_lsp').update_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+  ),
+  on_attach = function(_, _)
+    vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+  end
+}
+
+lcfg.util.default_config = vim.tbl_deep_extend(
+  'force',
+  lcfg.util.default_config,
+  lsp_defaults
+)
+
 lcfg.zls.setup{}
 lcfg.ccls.setup{}
 lcfg.gopls.setup{}
@@ -10,3 +28,5 @@ lcfg.html.setup{}
 lcfg.rnix.setup{}
 lcfg.vimls.setup{}
 lcfg.sumneko_lua.setup{}
+
+
