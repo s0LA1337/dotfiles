d97c868098e3d4880d067621e26cccfaed406d23
s0LA1337 <dreamer@neoncity.dev>
2022-05-31 18:44:36 +0000

    Update some stuff

--- nvim/lua/init_lsp.lua
+++ nvim/lua/init_lsp.lua
@@ -2,7 +2,7 @@ local lcfg = require('lspconfig')
 
 local lsp_defaults = {
   flags = {
-    debounce_text_changes = 150,
+    debounce_text_changes = 100,
   },
   capabilities = require('cmp_nvim_lsp').update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
